1ef07791da17713b6ec3a3136ee1911646ab1c75
TiaoTiao87 <47874872+TiaoTiao87@users.noreply.github.com>
2020-05-16 18:05:51 +0800

    Create util.c

---
+++ 05-compiler-run/util.c
@@ -0,0 +1,9 @@
+#include "util.h"
+
+int readText(char *fileName, char *text, int size) {
+  FILE *file = fopen(fileName, "r");
+  int len = fread(text, 1, size, file);
+  text[len] = '\0';
+  fclose(file);
+  return len;
+}
